71a69e82767fd4c771b5ee3e32978d6119abebcd
Leonardo Nunes <zleo465@gmail.com>
2022-02-04 14:31:44 -0200

    created Error html Page

---
+++ src/pages/Error/index.js
@@ -0,0 +1,15 @@
+import "./style.css";
+import { Link } from "react-router-dom";
+
+export default function Error(){
+    
+    return(
+        <div className="container-error">
+            <img src="notfound.png" alt="Pagina não encontrada" />
+            <h1>Pagina não encontrada</h1>
+            <Link to="/">
+            Voltar para home
+            </Link>
+        </div>
+    )
+}
