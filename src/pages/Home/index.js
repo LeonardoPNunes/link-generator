844730078a89b6fc7d65e9ee9c9d737b1f2e1748
Leonardo Nunes <zleo465@gmail.com>
2022-02-05 14:54:06 -0200

    adjustments

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,42 @@
-import { FiLink } from "react-icons/fi";
 import "./style.css";
+import { FiLink } from "react-icons/fi";
+import { useState } from "react";
+
+import Menu from "../../components/Menu";
+import LinkItem from "../../components/LinkItem";
+
+import api from "../../services/api";
+import { saveLink } from "../../services/storeLinks"; 
+
 export default function Home(){
+    const[link,setLink] = useState("");
+    const[showModal,setShowModal] = useState(false);
+    const [data,setData] = useState({});
+
+    async function HandleShortLink(){
+        try{
+            const response = await api.post("/shorten",{
+                long_url: link
+            })
+            setData(response.data);
+            setShowModal(true);
+
+            saveLink("@encurtaLink", response.data)
+
+            setLink("");
+
+        }catch(err){
+            alert("Ops parece que algo deu errado");
+            setLink("");
+        }
+    }
+
+    /*if showmodal === true return linkitem
+
+    {showModal &&(
+              <LinkItem />  
+            )}
+    */
 
     return(
         <div className="container-home">
@@ -12,10 +48,25 @@ export default function Home(){
             <div className="area-input">
                 <div>
                     <FiLink size={24} color="#FFF"/>
-                    <input type="text" />
+                    <input
+                     type="text"
+                     placeholder="Cole seu link aqui..."
+                     value={link}
+                     onChange={(e) => setLink(e.target.value)}
+                      />
                 </div>
-                <button>Encurtar Link</button>
+                <button onClick={HandleShortLink}>Encurtar Link</button>
             </div>
+            <Menu/>
+            
+            {showModal &&(
+              <LinkItem 
+              closeModal={() => setShowModal(false)}
+              content={data}
+              />  
+            )}
+
+            
         </div>
     )
 }
