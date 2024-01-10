aa1042e758ec44f7e97b27df998ee00eb2b98e52
Leonardo Nunes <85516883+LeonardoPNunes@users.noreply.github.com>
2024-01-09 23:45:27 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-export const key = "5b568e7b1d830d85934edc4df2f1e236493f9afa";
+export const key = "5f36e4973a4f6eae4e42ee3a8d34abae7510bac3";
 
 const api = axios.create({
-    baseURL:"https://api-ssl.bitly.com/v4/",
+    baseURL:"https://api-ssl.bitly.com/v4",
     headers:{
         "Authorization":`Bearer ${key}`
     }
